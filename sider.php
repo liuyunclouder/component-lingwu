43ab672da1d2bad8d6a98ceec11b276fd27d8314
灵吾 <lingwu.tyf@taobao.com>
2012-08-24 17:38:24 +0800

    finish sider.php

--- sider.php
+++ sider.php
@@ -1,6 +1,22 @@
-<div class="sider">
-	<ul class="item-list">
-		<li>recommend</li>
-		<li>loginpopup</li>
-	</ul>
-</div>
+<?php
+	// echo getcwd();
+	$path = dirname(__FILE__);
+	$handle = opendir($path);
+	$htmlStr = '<ul class="nav-list">';
+
+	while ($file = readdir($handle)) {
+		if ($file != '.' && $file != '..') {
+			if (is_dir($file) && $file != '.git') {
+				// echo "__". $file;
+				// echo "<br/>";
+				$htmlStr .= "<li><a href=\"index.php?page=$file\" >".$file."</a>";
+			}
+		}
+	}
+	$htmlStr .= '</ul>';
+	echo $htmlStr;
+	
+ ?>
+
+
+ 	
