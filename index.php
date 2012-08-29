d90d0e11a00e437f577848f6d6a2dae7b02cee8e
灵吾 <lingwu.tyf@taobao.com>
2012-08-29 14:06:24 +0800

    move sider.php content to index.php

--- index.php
+++ index.php
@@ -4,11 +4,27 @@
     $KISSYNum = '1.2.0';
     $page = $_GET['page'];
     $allowed_array = array();
-    require_once 'sider.php';
-    // $pa = Null;
-    // var_dump(empty($pa));
-    // echo is_bool(preg_grep("/index/i", $page));
-    // echo "<br />done<br /><br />";
+// 输出目录
+    $path = dirname(__FILE__);
+    $filter_array = array('.git', 'tool');
+    $handle = opendir($path);
+    $htmlStr = '<ul class="nav-list">';
+    
+
+    while ($file = readdir($handle)) {
+        if ($file != '.' && $file != '..') {
+            if (is_dir($file) && !in_array($file, $filter_array)) {
+                // echo "__". $file;
+                // echo "<br/>";
+                array_push($allowed_array, $file);
+                $htmlStr .= "<li><a href=\"index.php?page=$file\" >".$file."</a>";
+            }
+        }
+    }
+    $htmlStr .= '</ul>';
+    echo $htmlStr;
+
+// 引用相关文件
     if (isset($page) && in_array($page, $allowed_array)) {
     	include "$page/index.php";
     } else {
--- loginpopup/index.php
+++ loginpopup/index.php
@@ -1,4 +1,9 @@
 <?php
+	if (!isset($page)) {
+		$page = dirname(__FILE__);
+		// echo "$page";
+	}
+	
 	include $page.'/demo/demo.php';
     include $page.'/readme.md'; 
 ?>
